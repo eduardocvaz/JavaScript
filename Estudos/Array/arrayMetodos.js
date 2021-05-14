2d3914437ac1b186a39061b5d01fe63f62dd2afd
DAWERTON EDUARDO CARLOS VAZ <edwardvaz@ufrn.edu.br>
2021-05-14 10:50:55 -0300

    Array em JavaScript 1/2 concluido

---
+++ Estudos/Array/arrayMetodos.js
@@ -0,0 +1,30 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() //Massa quebrou o carro!
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento da lista!
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// splice pode adicionar ou remover elementos dentro do array
+
+//adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1) // massa quebrou :(
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2)
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
+
+
